cb4f44364a0ec3d2d51755c4764383579b1090d6
Benjamin <>
2019-05-06 22:41:58 -0400

    Added DNG

--- autogps.py
+++ autogps.py
@@ -56,11 +56,28 @@ def _backup(source_file: Path, backup_path: str) -> None:
 
 
 def _geotag_photos(gpx_file: Path, photos_path: Path) -> None:
+    # ORF
     # exiftool -geotag "$GPX_FILE" '-geotime<${DateTimeUTC}+00:00' -P *.ORF -srcfile %d%f.xmp -srcfile @ -o %d%f.xmp
     result = subprocess.run(" ".join(['exiftool', '-geotag',
                         '"' + str(gpx_file) + '"',
                         "'-geotime<${DateTimeUTC}+00:00'",
                         '-P', '*.ORF',
+                        '-srcfile', '%d%f.xmp',
+                        '-srcfile' , '@',
+                        '-o', '%d%f.xmp']),
+                        cwd=photos_path,
+                        capture_output=True,
+                        shell=True)
+    log.info(result)
+    # result.check_returncode()
+    log.info(result.stdout)
+
+    # DNG with camera set to UTC timezone
+    # exiftool -geotag "$GPX_FILE" '-geotime<${DateTimeUTC}+00:00' -P *.ORF -srcfile %d%f.xmp -srcfile @ -o %d%f.xmp
+    result = subprocess.run(" ".join(['exiftool', '-geotag',
+                        '"' + str(gpx_file) + '"',
+                        "'-geotime<${DateTimeOriginal}+00:00'",
+                        '-P', '*.DNG',
                         '-srcfile', '%d%f.xmp',
                         '-srcfile' , '@',
                         '-o', '%d%f.xmp']),
